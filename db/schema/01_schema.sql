72cf82cb1cf04e5bb13d9ba4944a68a6d776a86b
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-22 21:02:13 +0000

    (paired: Anit) created tables and populated

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,7 +1,35 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(20) NOT NULL
 );
+
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price INTEGER NOT NULL,
+  estimated_time INTEGER NOT NULL
+  thumbnail_photo_url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  accepted_at TIMESTAMP,
+  total_price INTEGER NOT NULL,
+  estimated_time INTERGER NOT NULL
+);
+
