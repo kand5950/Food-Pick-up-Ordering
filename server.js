63cb53e104e7c7760a0d1fb8f5346b6db5058559
Anit <anittankandasamy@gmail.com>
2022-10-22 20:31:03 -0400

    added get routed for users, restuarants, login, logout, homepage

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 
 // Web server config
 const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
+const express        = require('express');
+const morgan         = require('morgan');
+const cookieSession  = require('cookie-session');
+const bodyParser     = require('body-parser');
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+const PORT           = process.env.PORT || 8080;
+const app            = express();
 
 app.set('view engine', 'ejs');
 
@@ -25,28 +27,42 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: "session",
+  keys: ["id"]
+})
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
+const restuarantsRoutes = require('./routes/restuarants')
 const usersRoutes = require('./routes/users');
+const usersLoginRoutes = require('./routes/users_login');
+const usersLogoutRoutes = require('./routes/users_logout');
+const homepageRoutes = require('./routes/homepage');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
+app.use('/resuarants', restuarantsRoutes);
 app.use('/users', usersRoutes);
+app.use('/users_login', usersLoginRoutes);
+app.use('/users_logout', usersLogoutRoutes);
+app.use('/homepage', homepageRoutes)
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
