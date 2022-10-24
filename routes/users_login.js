9dd23c9b78d0935eb3943d58cf89771411494cad
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-24 22:46:44 +0000

    added post login route

--- routes/users_login.js
+++ routes/users_login.js
@@ -3,6 +3,8 @@ const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('user_login');
-}); 
-
+});
+router.post('/login', (req, res) => {
+  res.redirect('index');
+});
 module.exports = router;
