d4badc5702274c9b387832a86e7774f4ff6777a2
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-10-26 00:17:56 +0000

    login now tracks cookies

--- routes/users_login.js
+++ routes/users_login.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router  = express.Router();
+const db = require('../db/connection');
 
 router.get('/', (req, res) => {
   res.render('user_login');
 });
-router.post('/login', (req, res) => {
-  res.redirect('index');
+router.post('/', (req, res) => {
+  // get email from form
+  const email = req.body.email
+  console.log(email);
+  // find user id
+  db.query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((rows) => {
+    const user = rows.rows[0]
+     // set cookie
+    req.session.user_id = user.id
+    console.log(req.session.user_id)
+    res.redirect('index');
+  })
+  .catch((err) => console.log(err))
 });
+
 module.exports = router;
