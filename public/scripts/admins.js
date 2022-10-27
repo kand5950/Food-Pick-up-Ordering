aa03f1fee73d12a63d116fcaf02fb14ba20e9ae7
Anit <anittankandasamy@gmail.com>
2022-10-27 18:13:15 -0400

    starting of admin portal page to accept order

--- public/scripts/admins.js
+++ public/scripts/admins.js
@@ -1,2 +1,21 @@
+// Client facing scripts here for admin portal
+$(() => {
+    
+    $('.button1').on("click", function (event) {
+        console.log("cliked accept")
+        $("#collapseTwo").append(`
+        <div class="card-body">
+        <h2 class="header4">New Estimate time: <%= order.estimated_time %>min</h2>
 
+        <h2 class="header2">Placeholder for list of items: <%= order.order_food %></h2>
+  
+          <h2 class="header3">Total: $<%= order.total_price %></h2>
+      </div>
+      `);
+    });
+
+
+
+
+  });
 
