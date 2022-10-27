178a14d44c6b16bfc2b184bf84fc356e2756d1cb
Anit <anittankandasamy@gmail.com>
2022-10-27 19:58:01 -0400

    implemented new estimated time button

--- public/scripts/admins.js
+++ public/scripts/admins.js
@@ -1,15 +1,27 @@
 // Client facing scripts here for admin portal
 $(() => {
-    
+
+
+    $('#waitTime').change(function () {
+        selected = $(this).find("option:selected").text();
+      });
+
+
+    estTime = [];
+
     $('.button1').on("click", function (event) {
+        $("#collapseOne .card-body").empty();
+
+
+
         console.log("cliked accept")
         $("#collapseTwo").append(`
         <div class="card-body">
-        <h2 class="header4">New Estimate time: <%= order.estimated_time %>min</h2>
+        <h2 class="header4">New Estimate time: ${selected} min</h2>
 
-        <h2 class="header2">Placeholder for list of items: <%= order.order_food %></h2>
+        <h2 class="header2">List of items to be done: ... </h2>
   
-          <h2 class="header3">Total: $<%= order.total_price %></h2>
+          <h2 class="header3">Total: ... $</h2>
       </div>
       `);
     });
