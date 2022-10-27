b5c56d1932a06ef80ab77b0f041f4ced1a147b30
Anit <anittankandasamy@gmail.com>
2022-10-26 22:10:55 -0400

    implemented order to be saved to data base and showed on users confirmation page

--- routes/user_confirmation.js
+++ routes/user_confirmation.js
@@ -10,16 +10,25 @@ let order;
 router.get('/', (req, res) => {
   const templateVars = {};
   const user_id = req.session.user_id
-  order['customers_id'] = user_id
+  order['customer'] = user_id
   ordershelper.getOrder(user_id).then(order => {
+    console.log(order)
     templateVars["order"] = order;
     res.render('user_confirmation', templateVars)
   })
 
 });
 
+
 router.post('/', (req, res) => {
-  order = req.body;
+  console.log(Number(req.session.user_id))
+  console.log(req.body)
+  order = {user_id: req.session.user_id, order_food: req.body.foods_ordered.map(function(foods) { return foods["name"]}).join(", "), total_price: req.body.total, estimated_time: req.body.total}
+  ordershelper.createOrder(order).then(order => {
+    console.log(order)
+  })
+  // console.log(order.foods_ordered.map(function(foods) { return foods["name"]}).join(", ")) gets food from order
+  
   
 })
 
--- routes/users_menu.js
+++ routes/users_menu.js
@@ -5,6 +5,7 @@ const itemsHelper = require('../db/queries/menus')
 // console.log(itemsHelper)
 
 router.get('/', (req, res) => {
+  console.log(req.session.user_id)
   const templateVars = {};
   itemsHelper.getItems().then(items => {
     templateVars["menu"] = items;
