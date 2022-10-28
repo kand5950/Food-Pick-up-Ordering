e97fa3e4a8db43bb868e286454cfa4ad1760ce4e
Anit <anittankandasamy@gmail.com>
2022-10-27 20:32:06 -0400

    completed accept button. now database update next

--- public/scripts/admins.js
+++ public/scripts/admins.js
@@ -1,6 +1,7 @@
 // Client facing scripts here for admin portal
 $(() => {
 
+  const userOrder = JSON.parse(order);
 
     $('#waitTime').change(function () {
         selected = $(this).find("option:selected").text();
@@ -19,9 +20,9 @@ $(() => {
         <div class="card-body">
         <h2 class="header4">New Estimate time: ${selected} min</h2>
 
-        <h2 class="header2">List of items to be done: ... </h2>
+        <h2 class="header2">List of items to be done: ${userOrder.order_food} </h2>
   
-          <h2 class="header3">Total: ... $</h2>
+          <h2 class="header3">Total: ${userOrder.total_price} $</h2>
       </div>
       `);
     });
